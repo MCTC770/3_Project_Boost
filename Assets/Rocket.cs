2eb422af091c2097c21882cc81c9dde4538d1c2b
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-07-21 23:38:09 +0200

    Added rocket thrust sounds

---
+++ Assets/Rocket.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	Rigidbody rigidbodyRocket;
+	AudioSource rocketThrustSound;
+
+	// Use this for initialization
+	void Start () {
+		rigidbodyRocket = GetComponent<Rigidbody>();
+		rocketThrustSound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput();
+	}
+
+	private void ProcessInput()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			rigidbodyRocket.AddRelativeForce(Vector3.up);
+			if (!rocketThrustSound.isPlaying)
+			{
+				rocketThrustSound.Play();
+			}
+		}
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			rocketThrustSound.Pause();
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.forward);
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(Vector3.back);
+		}
+	}
+}
